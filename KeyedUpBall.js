cb829cb4daa8dedc6bd48495d678c6878bc0b572
dianarosenberger <fc4431@wayne.edu>
2016-03-10 18:02:19 -0500

    Get bewildered and clean up not working code.

--- KeyedUpBall.js
+++ KeyedUpBall.js
@@ -102,9 +102,10 @@ KeyedUpBall.prototype = {
 
   // checkForIntersection() should returns true if this ball intersects with another ball
   // for this to work, the other ball must have its position represented by a position object that has an x and y, as well as a radius, as our BouncyBall does
-  detectCollision: function(otherBall) {
-
-    // your code goes here
+detectCollision: function(otherBall) {
+  var distanceBetweenBalls = dist(this.position.x, this.position.y, otherBall.position.x, otherBall.position.y);
+  var collisionDistance = this.radius + otherBall.radius;
+  return (distanceBetweenBalls < collisionDistance);
 
   }
 
--- NotPacman.js
+++ NotPacman.js
@@ -1,3 +1,40 @@
+var timer = new Timer();
+
+var counter = {
+  number: 0,
+
+  display: function () {
+    textSize(40);
+    fill(255);
+    text(this.number, 350, 350);
+  },
+
+  update: function() {
+
+  },
+
+  addOneToScore:function(){
+      this.number++;
+      console.log("Increment score to " + this.number);
+  },
+
+};
+
+var balls = [];
+
+var startingBalls = 20;
+/*
+var initializeBall = function (ball, oneBall, array) {
+  ball.initialize();
+};
+
+var updateAndDisplay = function(ball, oneBall, array) {
+  ball.update();
+  ball.display();
+};
+
+*/
+
 /*
   NOT PACMAN GAME
   This is the object where you should write just about all your code for this assignment.
@@ -12,15 +49,45 @@ var NotPacmanGame = function () {
 NotPacmanGame.prototype = {
 
   initialize: function () {
-
-  },
+    createCanvas(400, 400);
+    while (balls.length < startingBalls) balls.push(new BouncyBall(width/2, height/2));
+    balls.forEach(initializeBall);
+    this.keyedUpBall = new KeyedUpBall(25, 25);
+    this.keyedUpBall.initialize();
+},
 
   update: function () {
+    this.keyedUpBall.update();
+    balls.forEach(this.checkBallToDelete, this);
+  },
+
+display: function () {
+      background(0);
+      text(timer.getPrettyElapsedTime(), 50, 350);
+      textSize(40);
+      fill(255);
+      balls.forEach(updateAndDisplay);
+      this.keyedUpBall.update();
+      this.keyedUpBall.display();
+      balls.forEach(this.updateAndDisplayBalls);
+      this.checkBallToDelete();
+      counter.display();
+      counter.addOneToScore();
+      console.log("Testing display.");
 
   },
 
-  display: function () {
-    
-  }
+  checkBallToDelete: function(otherBall, oneBall, array) {
+        if (this.keyedUpBall.detectCollision(otherBall)) this.deleteBallHere(oneBall);
+    },
 
+  deleteBallHere: function(oneBall) {
+        console.log("Deleting ball at " + oneBall);
+        balls.splice(oneBall, 1);
+    },
+
+  updateAndDisplayBalls: function(ball, index, array) {
+    ball.update();
+    ball.display();
+  },
 };
